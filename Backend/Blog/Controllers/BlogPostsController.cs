9881ba15a32853b253a000366a068bbff5c00baf
rodrigorangeldev <rodrigorangeldev@gmail.com>
2021-01-20 16:03:34 -0300

    enviando para o github

---
+++ Backend/Blog/Controllers/BlogPostsController.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Blog.Data;
+using Blog.Models;
+
+namespace Blog.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class BlogPostsController : ControllerBase
+    {
+        private readonly BlogPostsContext _context;
+        private readonly IDataRepository<BlogPost> _repo;
+
+        public BlogPostsController(BlogPostsContext context, IDataRepository<BlogPost> repo)
+        {
+            _context = context;
+            _repo = repo;
+        }
+
+        // GET: api/BlogPosts
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BlogPost>>> GetBlogPost()
+        {
+            return await _context.BlogPost.ToListAsync();
+        }
+
+        // GET: api/BlogPosts/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BlogPost>> GetBlogPost(int id)
+        {
+            var blogPost = await _context.BlogPost.FindAsync(id);
+
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+
+            return blogPost;
+        }
+
+        // PUT: api/BlogPosts/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBlogPost(int id, BlogPost blogPost)
+        {
+            if (id != blogPost.PostId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(blogPost).State = EntityState.Modified;
+
+            try
+            {
+                _repo.Update(blogPost);
+                var save = await _repo.SaveAsync(blogPost);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BlogPostExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/BlogPosts
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<BlogPost>> PostBlogPost(BlogPost blogPost)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _repo.Add(blogPost);
+            var save = await _repo.SaveAsync(blogPost);
+
+
+            return CreatedAtAction("GetBlogPost", new { id = blogPost.PostId }, blogPost);
+        }
+
+        // DELETE: api/BlogPosts/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBlogPost(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var blogPost = await _context.BlogPost.FindAsync(id);
+            if (blogPost == null)
+            {
+                return NotFound();
+            }
+
+            _repo.Delete(blogPost);
+            var save = _repo.SaveAsync(blogPost);
+
+            return Ok(blogPost);
+        }
+
+        private bool BlogPostExists(int id)
+        {
+            return _context.BlogPost.Any(e => e.PostId == id);
+        }
+    }
+}
